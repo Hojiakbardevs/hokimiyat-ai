6d4fe38f604b338532a51f8c5fd3c92bdb147bdc
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2025-11-03 19:49:27 +0500

    men qolganini uyda bajaraman nasib

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,16 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true, // Listen on all network interfaces (0.0.0.0)
+    port: 5173,
+    strictPort: false,
+    open: true, // Automatically open browser
   },
 })
